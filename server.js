6aea98f757e5fd00764c97333cfa1d46dc4ce1df
TLAMHutto <keatonhutto8@gmail.com>
2022-07-31 01:50:04 -0500

    first

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+
+//parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Server static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+//Set api routes
+// app.use('/api', api)
+
+//Return other routes to Aunger index files
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/code-post/index.html'));
+})
+//Set port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+//Create the http server
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Server running on port ${port}`));
